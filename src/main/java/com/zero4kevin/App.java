15799f34c28fc9d5e48d3cded7749453e90a24e3
zero4kevin <kevin21th@hotmail.com>
2017-06-18 23:06:35 +0800

    add new file

---
+++ src/main/java/com/zero4kevin/App.java
@@ -0,0 +1,12 @@
+package com.zero4kevin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/main/java/com/zero4kevin/collection/tour/Collections.java
@@ -0,0 +1,24 @@
+package com.zero4kevin.collection.tour;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by kevin on 6/18/17.
+ */
+public class Collections {
+    public static void main(String[] args){
+        LinkedList<String> linkedList=new LinkedList<String>();
+        linkedList.add("First Item");
+        linkedList.add("Second Item");
+        System.out.println(linkedList.removeFirst());
+        System.out.println("linkedList size is "+ linkedList.size());
+        Iterator<String> i=linkedList.iterator();
+        System.out.print("The left items are: ");
+        while(i.hasNext()){
+            System.out.print(i.next()+", ");
+        }
+        System.out.println();
+
+    }
+}
