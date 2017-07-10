46c8d300f12e014b26faca33bd5c81204b0f09d6
xi1zhang <kevin.zhang@nokia.com>
2017-07-10 17:59:38 +0800

    add examples of iterator pattern

---
+++ src/main/java/com/zero4kevin/desginPatterns/IteratorPattern/DinerMenu.java
@@ -0,0 +1,33 @@
+package com.zero4kevin.desginPatterns.IteratorPattern;
+
+/**
+ * Created by xi1zhang on 2017/7/10.
+ */
+public class DinerMenu {
+    static final int MAX_ITEMS= 6;
+    int numberOfItems=0;
+    MenuItem[] menuItems;
+
+    public DinerMenu(){
+        menuItems =new MenuItem[MAX_ITEMS];
+        addItem("Vegetarian BLT", "Bacon with lettuce & tomato on whole wheat", true, 2.99);
+        addItem("BLT", "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+        addItem("Hotdog", "A hot dog, with saurkraut, relish, oions, topped with cheese", false, 3.29);
+        addItem("Soup of the day", "Soup of the day, with a side of potato salad", false, 3.05);
+
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem=new MenuItem(name, description,vegetarian,price);
+        if(numberOfItems>=MAX_ITEMS){
+            System.out.println("Sorry, menu is full! Can't add item to menu");
+        } else {
+            menuItems[numberOfItems]=menuItem;
+            numberOfItems=numberOfItems+1;
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
